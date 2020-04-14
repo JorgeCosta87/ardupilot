1b939c497b1374cadecaf9b6459c03bef65426bd
Wallace Oliveira <wallace9655@gmail.com>
2020-04-14 05:44:10 +0100

    added type field to GetMissionWaypoints, to identify if the drone is going up, down or moving horizontally; Added enum constants for type

--- ArduCopter/GPS_Plot/LogUtils.py
+++ ArduCopter/GPS_Plot/LogUtils.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+from enum import IntEnum
 import os.path
 
 #Function to get Lattitude, Longitude and Altitude from file and gives it back in X, Y, Z list
@@ -21,9 +23,13 @@ def GetGPSData(filename):
     file.close();
 
     return x, y, z;
-    
 
-from collections import namedtuple
+
+class WPaction(IntEnum):
+    VERTICAL_UP     = 0
+    VERTICAL_DOWN   = 1
+    HORIZONTAL      = 2
+    
 
 def GetMissionWaypoints(filename, takeoffH = "nan", landingH = "nan"):
     if not os.path.isfile(filename):
@@ -82,7 +88,11 @@ def GetMissionWaypoints(filename, takeoffH = "nan", landingH = "nan"):
             data.y1 = float(split[8]);
             data.z1 = float(default.alt + float(split[10]));# TODO: this can screw up some stuff when there are more than 1 waypoint, needs testing
             
-            data.type = 2;
+            #if the drone is going up and down in the exact same position
+            if data.x == data.x1 and data.y == data.y1:
+                data.type = WPaction.VERTICAL
+            else:
+                data.type = WPaction.HORIZONTAL
 
 
         elif(action == takeoff or action == landing): #TODO: add RTL to this condition
@@ -97,11 +107,11 @@ def GetMissionWaypoints(filename, takeoffH = "nan", landingH = "nan"):
             data.y1 = last.y1;
             data.z1 = float(default.alt + float(split[10])); #TODO: comment later
 
-            if(action == landing and type(landingH) != type(str)):#TODO: needs to be documented.
+            data.type = WPaction.VERTICAL_UP
+
+            if(action == landing and type(landingH) != type(str)):
                 data.z1 = landingH;
-                data.type = 0;
-            else:
-                data.type = 1;
+                data.type = WPaction.VERTICAL_DOWN
 
         #points to last waypoint so that it's possible to calculate the next one
         last = data;
@@ -175,72 +185,3 @@ def getFaultyPoints(timestampFilename, coordinatesFilename):
     coordinates.close();
 
     return faults;
-
-
-
-"""
-#simple way to test GetMissionWaypoints
-import sys
-
-a = GetMissionWaypoints(sys.argv[1]);
-
-i = 1;
-#print "coord 0", d.lng, d.lat, d.alt;
-for point in a:
-    print "coord ", i , point.x, point.y, point.z, point.x1, point.y1, point.z1;
-    i = i+1;
-"""
-
-#This is the slowest thing to ever exist, it's just here for referece, do not use it, as it trashes performance
-"""
-def point_in_cylinder(pt1, pt2, points, r, N=100):
-    dist = np.linalg.norm(pt1 - pt2)
-    ori = (pt2 - pt1) / dist 
-    #line = np.array([pt1 + ori*t for t in np.linspace(0, dist, N)])
-    line = (pt1.reshape(3, 1) + elc_ori.reshape(3, 1) @ np.linspace(0, dist, N).reshape(1, N)).T
-    dists = np.min(cdist(line, points), 0)
-
-    return np.where(dists <= r)[0]
-"""
-
-#Better method O(1)
-#Given two 3d points and another list of 3d points, I want to check which one is inside the cylinder defined as the 3d line between the two points with a radius of r
-#q is the point to test
-def points_in_cylinder(pt1, pt2, r, q):
-    vec = pt2 - pt1
-    const = r * np.linalg.norm(vec)
-    return np.where(np.dot(q - pt1, vec) >= 0 and np.dot(q - pt2, vec) <= 0 
-           and np.linalg.norm(np.cross(q - pt1, vec)) <= const)
-
-"""
-import matplotlib.pyplot as plt
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import sys
-
-def test():
-    fig = plt.figure();
-    ax = fig.add_subplot(111, projection='3d');
-
-    dataset = GetMissionWaypoints(sys.argv[1]);
-    x = [];
-    y = [];
-    z = [];
-    for waypoint in dataset:
-    # Cylinder
-    #z.append()
-    Xc, Zc=np.meshgrid(x, z)
-    Yc = np.sqrt(1-Xc**2)
-
-    # Draw parameters
-    rstride = 20
-    cstride = 10
-    ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
-    ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride)
-
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
-    plt.show()
-"""
-#test();
