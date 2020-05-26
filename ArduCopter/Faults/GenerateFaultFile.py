4c610aeaf45e12943e80de76a883d381c1113a8b
Wallace Oliveira <wallace9655@gmail.com>
2020-05-26 16:33:45 +0100

    separated Enums from chart generator script; Automated some processes in GenerateFaultFile.py

--- ArduCopter/Faults/GenerateFaultFile.py
+++ ArduCopter/Faults/GenerateFaultFile.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python
+import sys
+sys.path.append('../Utils')
+
+from Enumerators import Sensor, Method
+
+# Constants for code clarity
+_DEVIATION  = 0
+_MEAN       = 1
+
+
+
+# Returns an array of arrays with the noise parameters for the Mean and Deviation
+# example: gerNoiseArrayCombination([ 0.5, 1.0, 5.0, 10.0 ], [ 0.5, 1.0, 5.0, 10.0 ])
+# output: [ [0.5, 0.5], [0.5, 1.0], [0.5, 5.0], [0.5, 10.0], [1.0, 0.5], ... , [ 10.0, 10.0 ]]
+# The arrays don't need to be the same, but for the sake of simplicity that's what was used for this example
+def getNoiseArrayCombination(arrayMean, arrayDeviation):
+    array = []
+    
+    for mean in arrayMean:
+        for deviation in arrayDeviation:
+            array.append([ deviation, mean ])
+
+    return array
 
 missions    = [ "straightLine.txt" ]
-methods     = [ 2 ]
-sensors     = [ 0, 1, 2, 3, 4 ]
-delays      = [ 0,15000 ]
-durations   = [ 100, 500, 1000, 10000, 120000 ]
-noises      = [ [0.0, 20.0], [0.0, 50.0], [0.0, 80.0], [0.0, 100.0], [20.0, 0.0], [50.0, 0.0],[80.0, 0.0], [100.0, 0.0] ]
-minvals     = [ 1 ]
-maxvals     = [ 100 ]
+methods     = [ Method.NOISE ]
+sensors     = [ Sensor.ACCELEROMETER, Sensor.COMPASS, Sensor.GYROSCOPE ]
+delays      = [ 0 ]
+durations   = [ 50, 100, 500, 1200000 ]
+noises      = getNoiseArrayCombination([ 0.5, 1.0, 5.0, 10.0 ], [ 1.0, 2.0, 5.0 ])
+minvals     = [ 0 ]
+maxvals     = [ 0 ]
 xyz_vals    = [ [0.0,0.0,0.0] ]
 radiuses    = [ 5 ]
 injc_on     = 1
@@ -29,7 +52,7 @@ for mission in missions:
                                                                     idcounter, injc_on, mission, radius,
                                                                     sensor, method, delay, duration, 
                                                                     trigger, xyz[0],xyz[1],xyz[2],
-                                                                    valmin, valmax, noise[0], noise[1])
+                                                                    valmin, valmax, noise[_DEVIATION], noise[_MEAN])
                                         )
                                         idcounter += 1
 
--- ArduCopter/Utils/GenerateOverviewPage.py
+++ ArduCopter/Utils/GenerateOverviewPage.py
@@ -10,6 +10,7 @@ import sys
 # Enum
 from enum import IntEnum
 from PointValidation import State
+from Enumerators import Sensor, Method
 
 # Data collections
 from collections import namedtuple
@@ -147,26 +148,7 @@ class ChartHandler:
         page += self._bodyBottom
 
         return page
-
-class Sensor(IntEnum):
-    COMPASS         = 0
-    GYROSCOPE       = 1
-    ACCELEROMETER   = 2
-    BAROMETER       = 3
-    TEMPERATURE     = 4
-    UNKNOWN         = 5
-    NONE            = 6
-
-class Method(IntEnum):
-    STATIC      = 0
-    RANDOM      = 1
-    NOISE       = 2
-    REPEAT_LAST = 3
-    DOUBLE_LAST = 4
-    HALF_LAST   = 5
-    MAX_VALUE   = 6
-    DOUBLE_MAX  = 7
-    MIN_VALUE   = 8
+        
 
 class Filter(IntEnum):
     ID          = 0
