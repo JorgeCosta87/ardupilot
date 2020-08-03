b21c73bb2b57d8f8e2be0a30d79ce4b226c4eb87
Wallace Oliveira <wallace9655@gmail.com>
2020-08-03 07:33:54 +0100

    Added scripts to re-evaluate the missions, as well as add new fields to the logs

---
+++ ArduCopter/Utils/DroneCrossedWaypoint.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import PointValidation as evaluation
+import LogUtils as utils
+import os
+from sys import argv
+
+if len(argv) < 3:
+    print "Example: ", argv[0], " <missionFile> < run gps file >"
+    exit(1)
+
+mission_path = argv[1]
+run = argv[2]
+
+#check if files are valid
+if not os.path.isfile(mission_path):
+    raise Exception('File\'' + mission_path + '\'does not exist')
+
+if not os.path.isfile(run):
+    raise Exception('File\'' + run + '\'does not exist')
+
+# Load mission and data 
+x,y,z = utils.GetGPSData(run)
+mission = utils.GetMissionWaypoints(mission_path)
+X = []; Y = []; Z = []
+for waypoint in mission:
+    X.append(waypoint.x)
+    X.append(waypoint.x1)
+    Y.append(waypoint.y)
+    Y.append(waypoint.y1)
+    Z.append(waypoint.z)
+    Z.append(waypoint.z1)
+
+sphere = (X[-2], Y[-2], Z[-2])
+eval = evaluation.Validation()
+for i in range(len(x)):
+    point = (x[i], y[i], z[i])
+    if eval._is_point_in_sphere(point, sphere, eval._meters_to_degrees(0.15)):
+        print True
+        exit(0)
+
+print False
