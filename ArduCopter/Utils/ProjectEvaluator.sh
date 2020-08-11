9ece816a45a689e0e296b642894c50501dca46a8
Wallace Oliveira <wallace9655@gmail.com>
2020-08-11 12:50:14 +0100

    Changed how stuck result is assigned Drone is considered stuck when it has been in the same spot for more than 15 seconds

--- ArduCopter/Utils/ProjectEvaluator.sh
+++ ArduCopter/Utils/ProjectEvaluator.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if (($# < 1)); then
+    printf "\033[1mUsage: $0 < Results.csv file >\033[0m\n";
+    exit 1
+fi
+
+cd "${0%/*}"
 mypath="$(dirname "$1")"
 
 writeResults(){
@@ -11,25 +18,79 @@ writeResults(){
     printf "$fullLine\n"
 }
 
+checkIfStuck(){
+    # Get last log line and line from 15 seconds ago
+    lastLine=$(tail -1 "$testFolder/gps.log" | head -1)
+    coordsInit=$(echo "$lastLine" | cut -d "," -f3,4,5)
+    
+    stuck=$(tail -75 "$testFolder/gps.log" | head -1)
+    coordsFinal=$(echo "$stuck" | cut -d "," -f3,4,5)
+
+    coords="$coordsInit,$coordsFinal"
+    
+    # Test if it's in the same position within a radius of 30 cm
+    stuck=$(./DroneCrossedWaypoint.py "$testFolder/gps.log" -c "$coords")
+}
+
+getMissionResult(){
+	local CRASH="CRASH"
+	local STUCK="STUCK"
+	local LOST_PATH="LOST_PATH"
+    local MAJOR_FAULT="MAJOR_FAULT"
+
+	crashCount=$(grep -i "crash" "$testFolder/console.log" | wc -l)
+	if (($crashCount > 0)); then
+		result=$CRASH
+		return
+	fi
+
+	# Read report and check if there was a timeout or not
+	{
+		IFS=","
+		read
+		read -ra pos
+		timeout=${pos[3]};
+	} < "$testFolder/simulations_report.csv"
+
+	# if there was a timeout
+	if [ "$timeout" == "Y" ]; then
+
+		# get mission evaluation
+		eval=$(./EvaluateMission.py "../Missions/$missionName"".txt" "$testFolder/gps.log")
+
+        # Check if the drone is stuck or lost path
+        checkIfStuck
+		if [ "$stuck" == "True" ]; then
+			result=$STUCK
+		else
+			result=$LOST_PATH
+		fi
+	else
+		result=$(./EvaluateMission.py "../Missions/$missionName"".txt" "$testFolder/gps.log")
+	fi
+}
+
 getTestFolder(){
     missionName="${3%.*}"
 	missionName="${missionName// /_}"
 
-    testFolder="$missionName""_$1/Run_$2"
+    testFolder="$mypath/$missionName""_$1/Run_$2"
+    if [ ! -d "$testFolder" ]; then
+        testFolder="$mypath/$missionName""_$1"
+    fi
 }
 
-handleNormal(){
-    getTestFolder "${array[0]}" "${array[1]}" "${array[3]}"
-    array[((${#array[@]}-1))]=$(./EvaluateMission.py "../Missions/$missionName"".txt" "$mypath/$testFolder/gps.log")
+evaluateRun(){
+    getMissionResult
+    array[((${#array[@]}-1))]="$result"
 }
 
 getLastWaypoint(){
-    getTestFolder "${array[0]}" "${array[1]}" "${array[3]}"
-    array[19]=$(./DroneCrossedWaypoint.py "../Missions/$missionName"".txt" "$mypath/$testFolder/gps.log")
+    array[19]=$(./DroneCrossedWaypoint.py "$testFolder/gps.log" -m "../Missions/$missionName"".txt")
 
     line=1
     while [ True ]; do
-        array[20]=$(tail -$line "$mypath/$testFolder/faults.csv" | head -1 | cut -d "," -f23)
+        array[20]=$(tail -$line "$testFolder/faults.csv" | head -1 | cut -d "," -f23)
 
         re='^[0-9]+$'
         if [[ ${array[20]} =~ $re ]] ; then
@@ -49,17 +110,21 @@ main(){
                 break;
             fi
 
-            #if [ ${array[18]} == "NORMAL" ]; then
-            #    handleNormal
-            #fi
+            getTestFolder "${array[0]}" "${array[1]}" "${array[3]}"
 
-            if [ "${array[18]//[$'\t\r\n ']}" == "CRASH" ]; then
-                getLastWaypoint
-            else
+            evaluateRun
+
+            #if [ ${array[18]} == "NORMAL" ]; then
+            #    evaluateRun
+            #fi
+            
+            #if [ "${array[18]//[$'\t\r\n ']}" == "CRASH" ]; then
+            #    getLastWaypoint
+            #else
                 #Hard coding at it's finest
-                array[19]="True"
-                array[20]="14"
-            fi
+            #    array[19]="True"
+            #    array[20]="14"
+            #fi
 
             writeResults
         done
